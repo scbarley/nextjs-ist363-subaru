79e0dd24c20501406f0688447b97e1780a3dc4a5
Simon Barley <scbarley@syr.edu>
2023-12-12 18:28:47 -0500

    did animation with framer

--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,9 +1,7 @@
 import Container from '../../components/Container';
 import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
-import Image from 'next/image';
 import Layout from '../../components/Layout';
-import Link from 'next/link';
 import { useState } from 'react';
 
 
@@ -50,31 +48,17 @@ const VehiclesPage = ({ vehicles, vehicleTypes })  => {
 
         <h1>Vehicles</h1>
         <Container>
+            
             <FilterBar 
-            items={vehicleTypes} 
-            activeItem={activeVehicleType}
-            setActiveItem={setActiveVehicleType}
+                items={vehicleTypes} 
+                activeItem={activeVehicleType}
+                setActiveItem={setActiveVehicleType}
             />
-            <Grid>
-                {filteredVehicles.map((vehicle, index) => {
-                    const {title, slug, vehicleInformation} = vehicle.node;
-                    const { trimLevels } = vehicleInformation;
-                    return <article key={index}>
-                        {trimLevels && trimLevels[0].images.thumbnail && 
-                            <Image 
-                                src={trimLevels[0].images.thumbnail.node.sourceUrl}
-                                alt={trimLevels[0].images.thumbnail.node.altText}
-                                width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
-                                height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
-                            />
-                        }
-                        <h3>{title}</h3>
-                        <p>
-                            <Link href={`/vehicles/${slug}`}>Learn more</Link>
-                        </p>
-                    </article>
-                })}
-            </Grid>
+
+            <Grid 
+                items={filteredVehicles} 
+            />
+            
         </Container>
     </Layout>
 };
